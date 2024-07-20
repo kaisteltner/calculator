dbd432c953ed62a860a888ef5410641bd35bdf88
Kai Steltner <steltner.kai@gmail.com>
2024-07-20 20:23:39 +0200

    feat: create empty math functions

--- javascript.js
+++ javascript.js
@@ -1,1 +1,21 @@
 console.log('Hello, calculator.');
+
+function add(a, b) {
+  // Add numbers a and b and return result.
+
+}
+
+function substract(a, b) {
+  // Substract number b from a and return result.
+
+}
+
+function multiply(a, b) {
+  // Multiply number a by b and return result.
+
+}
+
+function divide(a, b) {
+  // Divide number a by b and return result. Returns null for division by 0.
+  
+}
