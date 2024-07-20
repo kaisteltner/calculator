082d390be8ff05b2489ff3e2cf3dd4adf41132d1
Kai Steltner <steltner.kai@gmail.com>
2024-07-20 20:36:08 +0200

    feat: implement substract function

--- javascript.js
+++ javascript.js
@@ -1,11 +1,11 @@
 function add(a, b) {
   // Add numbers a and b and return result.
-  return a + b
+  return a + b;
 }
 
 function substract(a, b) {
   // Substract number b from a and return result.
-
+  return a - b;
 }
 
 function multiply(a, b) {
@@ -19,6 +19,11 @@ function divide(a, b) {
 }
 
 // Test add
-console.log("2 + 5 =", add(2, 5));
-console.log("2 + -5 =", add(2, -5));
-console.log("0.25 + 0.5 =", add(0.25, 0.5));
+// console.log("2 + 5 =", add(2, 5));
+// console.log("2 + -5 =", add(2, -5));
+// console.log("0.25 + 0.5 =", add(0.25, 0.5));
+
+// Test substract
+console.log("2 - 5 =", substract(2, 5));
+console.log("2 - -5 =", substract(2, -5));
+console.log("0.25 - 0.5 =", substract(0.25, 0.5));
