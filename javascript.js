609ff3e39f795e0b20db411700c8231190525e51
Kai Steltner <steltner.kai@gmail.com>
2024-08-05 10:29:35 +0200

    implement divide function

--- javascript.js
+++ javascript.js
@@ -15,7 +15,7 @@ function multiply(a, b) {
 
 function divide(a, b) {
   // Divide number a by b and return result. Returns null for division by 0.
-
+  if (b === 0) {return null} else {return a / b};
 }
 
 // Test add
@@ -29,7 +29,14 @@ function divide(a, b) {
 // console.log("0.25 - 0.5 =", substract(0.25, 0.5));
 
 // Test multiply
-console.log("2 * 5 =", multiply(2, 5));
-console.log("0 * -5 =", multiply(0, -5));
-console.log("-1 * -5 =", multiply(-1, -5));
-console.log("0.25 * -0.5 =", multiply(0.25, -0.5));
+// console.log("2 * 5 =", multiply(2, 5));
+// console.log("0 * -5 =", multiply(0, -5));
+// console.log("-1 * -5 =", multiply(-1, -5));
+// console.log("0.25 * -0.5 =", multiply(0.25, -0.5));
+
+// Test divide
+console.log("4 / 2 =", divide(4, 2));
+console.log("2 / -1 =", divide(2, -1));
+console.log("-1 / -5 =", divide(-1, -5));
+console.log("-1 / 0 =", divide(-1, 0));
+console.log("0.25 / -0.5 =", divide(0.25, -0.5));
